bd29f28dce0a00daa71fc34628b2e08fb935db3a
thaobone163 <71418685+thaobone163@users.noreply.github.com>
2022-06-26 18:51:24 +0700

    reconfig db

--- models/config.js
+++ models/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    host: 'db4free.net',
-    user: 'thaobone',
-    password: 'thaobone',
-    database: 'dictionary_app',
+    host: 'bbix2w5i3qnbhs1cudig-mysql.services.clever-cloud.com',
+    user: 'uhpyczcepthx9k3n',
+    password: '6o36CU5F1p6FeQCkdvq5',
+    database: 'bbix2w5i3qnbhs1cudig',
 }
